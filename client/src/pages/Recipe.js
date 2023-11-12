310ce3e3df0f561dbe974b065730ae74578fb889
Brian Teeters <brianteetersdesign@gmail.com>
2023-11-12 14:03:42 -0500

    comment update as submit, fields clear when submit

--- client/src/pages/Recipe.js
+++ client/src/pages/Recipe.js
@@ -6,6 +6,8 @@ function Recipe() {
     let {id} = useParams();
     const [recipeObject, setRecipeObject] = useState({});
     const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState("");
+    const [commentUsername, setUsername] = useState("");
 
     useEffect(() => {
         // Get data for specific recipe
@@ -19,8 +21,18 @@ function Recipe() {
         });
     }, []);
   
+    const addComment = () => {
+        axios.post("http://localhost:3001/comments", {commentBody: newComment, RecipeId: id, username: commentUsername}).then((response)=>{
+        const commentToAdd = {commentBody: newComment, username: commentUsername};    
+        setComments([...comments, commentToAdd]);
+        setNewComment("")
+        setUsername("")
+        });
+    };
+
     return (
         <div className='recipePage'>
+
             <div className='leftSide'>
                 <div className='recipeSection'>
                     <div className='title'>{recipeObject.title}</div>
@@ -28,17 +40,30 @@ function Recipe() {
                     <div className='footer'>{recipeObject.username}</div>
                 </div>
             </div>
+
             <div className='rightSide'>
                 <div>Ingredients Section</div>
             </div>
+
             <div className='commentsSection'>
                 <div className='addCommentContainer'>
-                    <input type='text' placeholder='Comment...' autoComplete='off'/>
-                    <button> Add Comment </button>
+                    <input 
+                    type='text' 
+                    placeholder='Comment...' 
+                    autoComplete='off' 
+                    value={newComment}
+                    onChange={(event)=>{setNewComment(event.target.value)}}/>
+                    <input 
+                    type='text' 
+                    placeholder='username' 
+                    autoComplete='off'
+                    value={commentUsername} 
+                    onChange={(event)=>{setUsername(event.target.value)}}/>
+                    <button onClick={addComment}> Add Comment </button>
                 </div>
                 <div className='listOfComments'>
                     {comments.map((comment, key) => {
-                        return <div key={key} className='comment'> {comment.commentBody} </div>
+                        return <div key={key} className='comment'> {comment.commentBody} - {comment.username} </div>
                     })}
 
                 </div>
